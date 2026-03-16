afad064bb9dd2dfe693e4413ee9744b78d4f5227
Avetis-Berberyan-OP <avetis.berberyan@optimumpartners.com>
2026-03-16 12:30:42 +0400

    minor fix

--- backend/server.ts
+++ backend/server.ts
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import documentRoutes from "./routes/documentRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", documentRoutes);
 app.use("/api", chatRoutes);
 
